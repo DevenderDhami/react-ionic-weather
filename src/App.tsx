4916f1da6c350bb30f7343587835af6f2f3b366a
Devender Dhami <devender.dhami.99@gmail.com>
2023-08-01 19:44:43 +0530

    weather added

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Redirect, Route } from 'react-router-dom';
+import {
+  IonApp,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  setupIonicReact
+} from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import { cloudy, ellipse, images, searchOutline, square, triangle } from 'ionicons/icons';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+import Weather from './pages/Weather/Weather';
+
+setupIonicReact();
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/weather">
+            <Weather />
+          </Route>
+          
+          <Route exact path="/">
+            <Redirect to="/weather" />
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/weather">
+            <IonIcon aria-hidden="true" icon={cloudy} />
+            <IonLabel>Weather</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
---
+++ src/pages/Weather/Weather.tsx
@@ -0,0 +1,86 @@
+import { IonButton, IonButtons, IonCol, IonContent, IonHeader, IonIcon, IonPage, IonRow, IonSearchbar, IonTitle, IonToolbar } from '@ionic/react';
+import { Geolocation } from '@capacitor/geolocation';
+import { useEffect, useState } from 'react';
+import { refreshOutline } from 'ionicons/icons';
+import { CurrentWeather } from '../../components/CurrentWeather/CurrentWeather';
+import { SkeletonDashboard } from '../../components/SkeletonDashboard/SkeletonDashboard';
+
+const Weather = () => {
+  const [search, setSearch] = useState("");
+  const [currentWeather, setCurrentWeather] = useState(false);
+
+  const performSearch = async () => {
+    getSearchedAddress(search);
+  }
+
+  const getSearchedAddress = async (city: any) => {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=f93eb660b2424258bf5155016210712&q=${city}&aqi=no`);
+    const data = await response.json();
+    if (data && data.current && data.location) {
+      setCurrentWeather(data);
+    }
+  }
+
+  useEffect(() => {
+    getCurrentPosition();
+  }, []);
+
+  const getCurrentPosition = async () => {
+    setCurrentWeather(false);
+    const coordinates = await Geolocation.getCurrentPosition();
+    getAddress(coordinates.coords);
+  }
+
+  const getAddress = async (coords: any) => {
+    const query = `${coords.latitude},${coords.longitude}`;
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=f93eb660b2424258bf5155016210712&q=${query}`);
+    const data = await response.json();
+    console.log(data);
+    setCurrentWeather(data);
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Weather</IonTitle>
+          <IonButtons slot="end">
+            <IonButton onClick={() => getCurrentPosition()}>
+              <IonIcon icon={refreshOutline} color="primary" />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Dashboard</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonRow className="ion-justify-content-center ion-margin-top ion-align-items-center">
+          <IonCol size="7">
+            <IonSearchbar placeholder="eg: delhi" animated value={search} onIonChange={(e: any) => setSearch(e.target.value)} />
+          </IonCol>
+
+          <IonCol size="5">
+            <IonButton expand="block" className="ion-margin-start ion-margin-end" onClick={performSearch}>Search</IonButton>
+          </IonCol>
+        </IonRow>
+
+        <IonRow className="ion-margin-start ion-margin-end ion-justify-content-center ion-text-center">
+          <IonCol size="12">
+            <h4>Here's your location based weather</h4>
+          </IonCol>
+        </IonRow>
+
+        <div style={{ marginTop: "-1.5rem" }}>
+          {currentWeather ? <CurrentWeather currentWeather={currentWeather} /> : <SkeletonDashboard />}
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Weather;
